bc8ee09c732eb78564fd55b467d0bb36a1ffe920
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-11-02 02:00:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array) {
+  array.forEach((element, index, array) => {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  var candy = ['kit kat', 'skittles', 'snickers'];
+
+  candy.forEach(callback);
+  return candy;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
